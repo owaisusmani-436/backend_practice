5bd004385b4e36e1f44dd60d98afba11209fb405
owaisusmani-436 <owaisusmani556677@gmail.com>
2026-01-27 00:31:38 +0530

    utils and some more written

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.db.js";
+import {app} from "./app.js"
 
 dotenv.config({
   path: "./.env",
 });
 
 connectDB()
+.then(() => { 
+  app.listen(process.env.PORT || 5000 , ()=> {
+    console.log(`server is running at port ${process.env.PORT}`);
+  })
+})
+.catch((err) => { 
+  console.log("mongodb connection failed" , err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // this ()() is iffy means function is going to xecute imediately
 /*
