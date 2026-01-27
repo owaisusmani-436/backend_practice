77bff834ccae79efb742db92362ba348b8120663
owaisusmani-436 <owaisusmani556677@gmail.com>
2026-01-27 15:06:27 +0530

    file uplaod and multer config

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        // upload file
+        const response = await cloudinary.uploader.upload(localFilePath , {
+            resource_type : "auto"
+        })
+
+        //file has been uplaoded 
+        console.log("file is uploaded successfully" , response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // removes locally saved temporary file if the upload op gets failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
