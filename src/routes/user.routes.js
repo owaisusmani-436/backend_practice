3a16cb50764262fca6df66b1299941be1ab94dcc
owaisusmani-436 <owaisusmani556677@gmail.com>
2026-02-06 19:23:54 +0530

    added new routes for user controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUsercoverImage,
+  getUSerChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controllers.js";
 import { upload} from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -31,5 +38,22 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUsercoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUSerChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchHistory)
+
+
 
 export default router
